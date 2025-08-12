243ad8a3d5b8a94ca903f4ce4a94e406b30add0a
João Paulo Vieira de Souza Montenegro <joaopaulovsm@gmail.com>
2025-08-11 22:53:14 -0300

    adicionando bibliotecas

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Dashboard de Salários na Área de Dados",
+    page_icon="📊",
+    layout="wide",
+)
+
+
+
